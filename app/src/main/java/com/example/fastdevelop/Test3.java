8dd9e4346d321ad78a0ba3a2b3098039e7ca56a1
gallop <1274224114@qq.ccom>
2023-05-08 20:58:20 +0800

    2023.05.08 完善商品详情界面

--- app/src/main/java/com/example/fastdevelop/Test3.java
+++ app/src/main/java/com/example/fastdevelop/Test3.java
@@ -3,6 +3,11 @@ package com.example.fastdevelop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.DynamicDrawableSpan;
+import android.text.style.ImageSpan;
+import android.widget.TextView;
 
 public class Test3 extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class Test3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_details);
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        SpannableString spannableString = new SpannableString(this.getString(R.string.large_text));
+        ImageSpan imageSpan = new ImageSpan(this, R.drawable.ic_right_next, DynamicDrawableSpan.ALIGN_CENTER);
+        spannableString.setSpan(imageSpan, 4, 6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(spannableString);
+
     }
 }
